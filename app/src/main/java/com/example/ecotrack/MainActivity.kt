ce76efdf087f091ba559351b4a8b5474535610b5
Your Name <your_email@example.com>
2025-12-09 22:28:14 +0500

    feat: Finalized Hilt setup and base Compose navigation

--- app/src/main/java/com/example/ecotrack/MainActivity.kt
+++ app/src/main/java/com/example/ecotrack/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.navigation.compose.rememberNavController
 import com.example.ecotrack.ui.theme.EcoTrackTheme
 import dagger.hilt.android.AndroidEntryPoint
 import androidx.compose.ui.unit.dp
+import com.example.ecotrack.ui.screens.home.HabitListScreen
 
 @AndroidEntryPoint // Внедрение зависимостей в Activity
 class MainActivity : ComponentActivity() {
@@ -48,7 +49,7 @@ fun EcoTrackAppEntryPoint() {
         ) {
             // Маршрут для экрана Алена
             composable("home") {
-                PlaceholderScreen("Home (Habit List)")
+                HabitListScreen()
             }
             // Маршрут для экрана статистики
             composable("statistics") {
